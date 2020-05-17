73c9b320e819adcf0493276a1c5ac36fec3dd41e
Konstantinos Loukas <constantine.loukas@gmail.com>
2020-05-17 10:51:50 +0200

    Fixes Formatting to adhere to CI4 Standards

--- src/CronExpression.php
+++ src/CronExpression.php
@@ -4,195 +4,204 @@ use CodeIgniter\I18n\Time;
 
 class CronExpression
 {
-    /**
-     * The timezone this should be considered under.
-     *
-     * @var string
-     */
-    protected $timezone;
+	/**
+	 * The timezone this should be considered under.
+	 *
+	 * @var string
+	 */
+	protected $timezone;
 
-    /**
-     * The current date/time. Used for testing.
-     *
-     * @var \DateTime
-     */
-    protected $testTime;
+	/**
+	 * The current date/time. Used for testing.
+	 *
+	 * @var \DateTime
+	 */
+	protected $testTime;
 
-    /**
-     * Allows us to set global timezone for all tasks
-     * on construct
-     * 
-     * @param string $timezone
-     * 
-     * @return void
-     */
-    public function __construct(string $timezone = null)
-    {
-        if($timezone === null){
-            $timezone = app_timezone();
-        }
-        
-        $this->timezone($timezone);
+	/**
+	 * Allows us to set global timezone for all tasks
+	 * on construct
+	 *
+	 * @param string $timezone The global timezone for all tasks
+	 *
+	 * @return void
+	 */
+	public function __construct(string $timezone = null)
+	{
+		if ($timezone === null)
+		{
+			$timezone = app_timezone();
+		}
 
-    }
+		$this->timezone($timezone);
+	}
 
-    /**
-     * Checks whether cron expression should be run. Allows
-     * for custom timezone to be used for specific task
-     * 
-     * @param string $expression
-     * @param string $timezone
-     * 
-     * @return void
-     */
-    public function shouldRun(string $expression, $timezone = null): bool
-    {
+	/**
+	 * Checks whether cron expression should be run. Allows
+	 * for custom timezone to be used for specific task
+	 *
+	 * @param string $expression The Cron Expression to be evaluated
+	 * @param string $timezone   The timezone for this specific task
+	 *
+	 * @return boolean
+	 */
+	public function shouldRun(string $expression, string $timezone = null): bool
+	{
+		// Set our current time
+		if (! $this->testTime instanceof Time)
+		{
+			$this->testTime = Time::now($this->timezone);
+		}
 
-        // Set our current time
-        if (! $this->testTime instanceof Time) {
-            $this->testTime = Time::now($this->timezone);
-        }
+		if (! is_null($timezone))
+		{
+			// Convert time object to selected timezone
+			$runtimeTimezone = new \DateTimeZone($timezone);
+		}
+		else
+		{
+			$runtimeTimezone = $this->timezone;
+		}
 
-        if(!is_null($timezone)){
-            // Convert time object to selected timezone
-            $runtimeTimezone = new \DateTimeZone($timezone);
-            
-        }else{
-            $runtimeTimezone = $this->timezone;
-        }
+		$this->testTime = $this->testTime->setTimezone($runtimeTimezone);
 
-        $this->testTime = $this->testTime->setTimezone($runtimeTimezone);
-        
+		// Break the expression into separate parts
+		[
+			$min,
+			$hour,
+			$monthDay,
+			$month,
+			$weekDay,
+		] = explode(' ', $expression);
 
-        // Break the expression into separate parts
-        [$min, $hour, $monthDay, $month, $weekDay] = explode(' ', $expression);
+		return $this->checkMinute($min)
+			&& $this->checkHour($hour)
+			&& $this->checkMonthDay($monthDay)
+			&& $this->checkMonth($month)
+			&& $this->checkWeekDay($weekDay);
+	}
 
-        return $this->checkMinute($min)
-            && $this->checkHour($hour)
-            && $this->checkMonthDay($monthDay)
-            && $this->checkMonth($month)
-            && $this->checkWeekDay($weekDay);
-    }
+	/**
+	 * Returns a Time instance representing the next
+	 * date/time this expression would be ran.
+	 *
+	 * @param string $expression
+	 *
+	 * @return Time
+	 */
+	public function nextRun(string $expression): Time
+	{
+	}
 
-    /**
-     * Returns a Time instance representing the next
-     * date/time this expression would be ran.
-     *
-     * @param string $expression
-     *
-     * @return Time
-     */
-    public function nextRun(string $expression): Time
-    {
+	/**
+	 * Returns a Time instance representing the last
+	 * date/time this expression would have ran.
+	 *
+	 * @param string $expression
+	 *
+	 * @return Time
+	 */
+	public function lastRun(string $expression): Time
+	{
+	}
 
-    }
+	/**
+	 * Sets the timezone that dates should be examined under.
+	 *
+	 * @param string $timezone
+	 *
+	 * @return $this
+	 * @throws \Exception Thrown if $timezone is not a valid timezone string.
+	 */
+	public function timezone(string $timezone)
+	{
+		// Throws exception if invalid Timezone is given
+		$this->timezone = new \DateTimeZone($timezone);
+		return $this;
+	}
 
-    /**
-     * returns a Time instance representing the last
-     * date/time this expression would have ran.
-     *
-     * @param string $expression
-     *
-     * @return Time
-     */
-    public function lastRun(string $expression): Time
-    {
+	/**
+	 * Sets a date/time that will be used in place
+	 * of the current time to help with testing.
+	 *
+	 * @param string $dateTime
+	 *
+	 * @return $this
+	 * @throws \Exception
+	 */
+	public function testTime(string $dateTime)
+	{
+		$this->testTime = Time::parse($dateTime, $this->timezone);
 
-    }
+		return $this;
+	}
 
-    /**
-     * Sets the timezone that dates should be examined under.
-     *
-     * @param string $timezone
-     *
-     * @return $this
-     * @throws \Exception
-     */
-    public function timezone(string $timezone)
-    {
-        // Throws exception if invalid Timezone is given
-        $this->timezone = new \DateTimeZone($timezone);
-        return $this;
-    }
+	protected function checkMinute(string $time): bool
+	{
+		return $this->checkTime($time, 'i');
+	}
 
-    /**
-     * Sets a date/time that will be used in place
-     * of the current time to help with testing.
-     *
-     * @param string $dateTime
-     *
-     * @return $this
-     * @throws \Exception
-     */
-    public function testTime(string $dateTime)
-    {
-        $this->testTime = Time::parse($dateTime,$this->timezone);
+	protected function checkHour(string $time): bool
+	{
+		return $this->checkTime($time, 'G');
+	}
 
-        return $this;
-    }
+	protected function checkMonthDay(string $time): bool
+	{
+		return $this->checkTime($time, 'j');
+	}
 
-    protected function checkMinute(string $time): bool
-    {
-        return $this->checkTime($time, 'i');
-    }
+	protected function checkMonth(string $time): bool
+	{
+		return $this->checkTime($time, 'n');
+	}
 
-    protected function checkHour(string $time): bool
-    {
-        return $this->checkTime($time, 'G');
-    }
+	protected function checkWeekDay(string $time): bool
+	{
+		return $this->checkTime($time, 'w');
+	}
 
-    protected function checkMonthDay(string $time): bool
-    {
-        return $this->checkTime($time, 'j');
-    }
+	/**
+	 * @param string $time
+	 * @param string $format
+	 *
+	 * @return boolean
+	 */
+	protected function checkTime(string $time, string $format): bool
+	{
+		if ($time === '*')
+		{
+			return true;
+		}
 
-    protected function checkMonth(string $time): bool
-    {
-        return $this->checkTime($time, 'n');
-    }
+		$currentTime = $this->testTime->format($format);
 
-    protected function checkWeekDay(string $time): bool
-    {
-        return $this->checkTime($time, 'w');
-    }
+		// Handle repeating times (i.e. /5 for every 5 minutes)
+		if (strpos($time, '/') === 0)
+		{
+			$period = substr($time, 1);
+			return ($currentTime % $period) === 0;
+		}
+		// Handle ranges (1-5)
+		elseif (strpos($time, '-') !== false)
+		{
+			$items = [];
+			[
+				$start,
+				$end,
+			]      = explode('-', $time);
 
-    /**
-     * @param string $time
-     *
-     * @return bool
-     */
-    protected function checkTime(string $time, string $format): bool
-    {
-        if ($time == '*')
-        {
-            return true;
-        }
+			for ($i = $start; $i <= $end; $i++)
+			{
+				$items[] = $i;
+			}
+		}
+		// Handle multiple days
+		else
+		{
+			$items = explode(',', $time);
+		}
 
-        $currentTime = $this->testTime->format($format);
-
-        // Handle repeating times (i.e. /5 for every 5 minutes)
-        if (strpos($time, '/') === 0)
-        {
-            $period = substr($time, 1);
-            return ($currentTime % $period) === 0;
-        }
-        // Handle ranges (1-5)
-        elseif (strpos($time, '-') !== false)
-        {
-            $items = [];
-            [$start, $end] = explode('-', $time);
-
-            for ($i = $start; $i <= $end; $i++)
-            {
-                $items[] = $i;
-            }
-        }
-        // Handle multiple days
-        else
-        {
-            $items = explode(',', $time);
-        }
-
-        return in_array($currentTime, $items);
-    }
+		return in_array($currentTime, $items);
+	}
 }
