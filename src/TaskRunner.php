99825a8e9f596755bc2d9ed55f8f5c26c2af0e7d
Lonnie Ezell <lonnieje@gmail.com>
2020-12-08 23:47:38 -0600

    Added basic logic to TaskRunner::run(). Needs tests

--- src/TaskRunner.php
+++ src/TaskRunner.php
@@ -1,19 +1,51 @@
 <?php namespace CodeIgniter\Tasks;
 
+use CodeIgniter\CLI\CLI;
+
+/**
+ * Class TaskRunner
+ *
+ * @package CodeIgniter\Tasks
+ */
 class TaskRunner
 {
-    /**
-     * @var Scheduler
-     */
-    protected $scheduler;
+	/**
+	 * @var Scheduler
+	 */
+	protected $scheduler;
 
-    public function __construct()
-    {
-        $this->scheduler = service('scheduler');
-    }
+	public function __construct()
+	{
+		$this->scheduler = service('scheduler');
+	}
 
-    public function run()
-    {
+	/**
+	 * The main entry point to run tasks within the system.
+	 * Also handles collecting output and sending out
+	 * notifications as necessary.
+	 */
+	public function run()
+	{
+		$tasks = $this->scheduler->getTasks();
 
-    }
+		if (! count($tasks))
+		{
+			return;
+		}
+
+		try
+		{
+			foreach ($tasks as $task)
+			{
+				if ($task->shouldRun())
+				{
+					$task->run();
+				}
+			}
+		}
+		catch (\Throwable $e)
+		{
+			log_message('error', $e->getMessage(), $e->getTrace());
+		}
+	}
 }
