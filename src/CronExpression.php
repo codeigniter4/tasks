1fcade8ab516954f9cc373df4a219657e9789df1
Lonnie Ezell <lonnieje@gmail.com>
2021-06-22 00:28:00 -0500

    initial work on next run calculator.

--- src/CronExpression.php
+++ src/CronExpression.php
@@ -1,176 +1,186 @@
-<?php namespace CodeIgniter\Tasks;
+<?php
+
+namespace CodeIgniter\Tasks;
 
 use CodeIgniter\I18n\Time;
 
 class CronExpression
 {
-	/**
-	 * The timezone this should be considered under.
-	 *
-	 * @var string
-	 */
-	protected $timezone;
+    /**
+     * The timezone this should be considered under.
+     *
+     * @var string
+     */
+    protected $timezone;
 
-	/**
-	 * The current date/time. Used for testing.
-	 *
-	 * @var \DateTime
-	 */
-	protected $testTime;
+    /**
+     * The current date/time. Used for testing.
+     *
+     * @var \DateTime
+     */
+    protected $testTime;
 
-	/**
-	 * Allows us to set global timezone for all tasks
-	 * on construct
-	 *
-	 * @param string $timezone The global timezone for all tasks
-	 *
-	 * @return void
-	 */
-	public function __construct(string $timezone = null)
-	{
-		$this->timezone = $timezone ?? app_timezone();
-	}
+    /**
+     * Allows us to set global timezone for all tasks
+     * on construct
+     *
+     * @param string $timezone The global timezone for all tasks
+     *
+     * @return void
+     */
+    public function __construct(string $timezone = null)
+    {
+        $this->timezone = $timezone ?? app_timezone();
+    }
 
-	/**
-	 * Checks whether cron expression should be run. Allows
-	 * for custom timezone to be used for specific task
-	 *
-	 * @param string $expression The Cron Expression to be evaluated
-	 *
-	 * @return boolean
-	 */
-	public function shouldRun(string $expression): bool
-	{
-		// Set our current time
-		if (! $this->testTime instanceof Time)
-		{
-			$this->testTime = Time::now($this->timezone);
-		}
+    /**
+     * Checks whether cron expression should be run. Allows
+     * for custom timezone to be used for specific task
+     *
+     * @param string $expression The Cron Expression to be evaluated
+     *
+     * @return boolean
+     */
+    public function shouldRun(string $expression): bool
+    {
+        $this->setTime();
 
-		// Break the expression into separate parts
-		[
-			$min,
-			$hour,
-			$monthDay,
-			$month,
-			$weekDay,
-		] = explode(' ', $expression);
+        // Break the expression into separate parts
+        [
+            $min,
+            $hour,
+            $monthDay,
+            $month,
+            $weekDay,
+        ] = explode(' ', $expression);
 
-		return $this->checkMinute($min)
-			&& $this->checkHour($hour)
-			&& $this->checkMonthDay($monthDay)
-			&& $this->checkMonth($month)
-			&& $this->checkWeekDay($weekDay);
-	}
+        return $this->checkMinute($min)
+            && $this->checkHour($hour)
+            && $this->checkMonthDay($monthDay)
+            && $this->checkMonth($month)
+            && $this->checkWeekDay($weekDay);
+    }
 
-	/**
-	 * Returns a Time instance representing the next
-	 * date/time this expression would be ran.
-	 *
-	 * @param string $expression
-	 *
-	 * @return Time
-	 */
-	public function nextRun(string $expression): Time
-	{
-		return new Time();
-	}
+    /**
+     * Returns a Time instance representing the next
+     * date/time this expression would be ran.
+     *
+     * @param string $expression
+     *
+     * @return Time
+     */
+    public function nextRun(string $expression): Time
+    {
+        $this->setTime();
 
-	/**
-	 * Returns a Time instance representing the last
-	 * date/time this expression would have ran.
-	 *
-	 * @param string $expression
-	 *
-	 * @return Time
-	 */
-	public function lastRun(string $expression): Time
-	{
-		return new Time();
-	}
+        return (new RunResolver())->nextRun($expression, clone($this->testTime));
+    }
 
-	/**
-	 * Sets a date/time that will be used in place
-	 * of the current time to help with testing.
-	 *
-	 * @param string $dateTime
-	 *
-	 * @return $this
-	 * @throws \Exception
-	 */
-	public function testTime(string $dateTime)
-	{
-		$this->testTime = Time::parse($dateTime, $this->timezone);
+    /**
+     * Returns a Time instance representing the last
+     * date/time this expression would have ran.
+     *
+     * @param string $expression
+     *
+     * @return Time
+     */
+    public function lastRun(string $expression): Time
+    {
+        return new Time();
+    }
 
-		return $this;
-	}
+    /**
+     * Sets a date/time that will be used in place
+     * of the current time to help with testing.
+     *
+     * @param string $dateTime
+     *
+     * @return $this
+     * @throws \Exception
+     */
+    public function testTime(string $dateTime)
+    {
+        $this->testTime = Time::parse($dateTime, $this->timezone);
 
-	protected function checkMinute(string $time): bool
-	{
-		return $this->checkTime($time, 'i');
-	}
+        return $this;
+    }
 
-	protected function checkHour(string $time): bool
-	{
-		return $this->checkTime($time, 'G');
-	}
+    private function checkMinute(string $time): bool
+    {
+        return $this->checkTime($time, 'i');
+    }
 
-	protected function checkMonthDay(string $time): bool
-	{
-		return $this->checkTime($time, 'j');
-	}
+    private function checkHour(string $time): bool
+    {
+        return $this->checkTime($time, 'G');
+    }
 
-	protected function checkMonth(string $time): bool
-	{
-		return $this->checkTime($time, 'n');
-	}
+    private function checkMonthDay(string $time): bool
+    {
+        return $this->checkTime($time, 'j');
+    }
 
-	protected function checkWeekDay(string $time): bool
-	{
-		return $this->checkTime($time, 'w');
-	}
+    private function checkMonth(string $time): bool
+    {
+        return $this->checkTime($time, 'n');
+    }
 
-	/**
-	 * @param string $time
-	 * @param string $format
-	 *
-	 * @return boolean
-	 */
-	protected function checkTime(string $time, string $format): bool
-	{
-		if ($time === '*')
-		{
-			return true;
-		}
+    private function checkWeekDay(string $time): bool
+    {
+        return $this->checkTime($time, 'w');
+    }
 
-		$currentTime = $this->testTime->format($format);
+    /**
+     * @param string $time
+     * @param string $format
+     *
+     * @return boolean
+     */
+    private function checkTime(string $time, string $format): bool
+    {
+        if ($time === '*') {
+            return true;
+        }
 
-		// Handle repeating times (i.e. /5 for every 5 minutes)
-		if (strpos($time, '/') === 0)
-		{
-			$period = substr($time, 1);
-			return ($currentTime % $period) === 0;
-		}
-		// Handle ranges (1-5)
-		elseif (strpos($time, '-') !== false)
-		{
-			$items = [];
-			[
-				$start,
-				$end,
-			]      = explode('-', $time);
+        $currentTime = $this->testTime->format($format);
 
-			for ($i = $start; $i <= $end; $i++)
-			{
-				$items[] = $i;
-			}
-		}
-		// Handle multiple days
-		else
-		{
-			$items = explode(',', $time);
-		}
+        // Handle repeating times (i.e. /5 for every 5 minutes)
+        if (strpos($time, '/') === 0) {
+            $period = substr($time, 1);
+            return ($currentTime % $period) === 0;
+        }
+        // Handle ranges (1-5)
+        elseif (strpos($time, '-') !== false) {
+            $items = [];
+            [
+                $start,
+                $end,
+            ]      = explode('-', $time);
 
-		return in_array($currentTime, $items);
-	}
+            for ($i = $start; $i <= $end; $i++) {
+                $items[] = $i;
+            }
+        }
+        // Handle multiple days
+        else {
+            $items = explode(',', $time);
+        }
+
+        return in_array($currentTime, $items);
+    }
+
+    /**
+     * Sets the current time if it hasn't already been set.
+     *
+     * @throws \Exception
+     */
+    private function setTime()
+    {
+        // Set our current time
+        if ($this->testTime instanceof Time) {
+            return;
+        }
+
+        $this->testTime = Time::now($this->timezone);
+    }
 }
--- tests/unit/CronExpressionTest.php
+++ tests/unit/CronExpressionTest.php
@@ -5,177 +5,214 @@ use CodeIgniter\Test\CIUnitTestCase as TestCase;
 
 class CronExpressionTest extends TestCase
 {
-	/**
-	 * @var CronExpression
-	 */
-	protected $cron;
+    /**
+     * @var CronExpression
+     */
+    protected $cron;
 
-	public function setUp(): void
-	{
-		parent::setUp();
+    public function setUp(): void
+    {
+        parent::setUp();
 
-		$this->cron = new CronExpression();
-	}
+        $this->cron = new CronExpression();
+    }
 
-	public function testMinutes()
-	{
-		$this->assertTrue($this->cron->shouldRun('* * * * *'));
+    public function testMinutes()
+    {
+        $this->assertTrue($this->cron->shouldRun('* * * * *'));
 
-		$this->cron->testTime('2020-05-01 10:04 am');
-		$this->assertFalse($this->cron->shouldRun('10 * * * *'));
-		$this->assertTrue($this->cron->shouldRun('4 * * * *'));
-		$this->assertTrue($this->cron->shouldRun('04 * * * *'));
-		$this->assertTrue($this->cron->shouldRun('4,8 * * * *'));
-		$this->assertTrue($this->cron->shouldRun('1,2,4 * * * *'));
-		$this->assertFalse($this->cron->shouldRun('5-15 * * * *'));
-		$this->assertTrue($this->cron->shouldRun('1-5 * * * *'));
-		$this->assertTrue($this->cron->shouldRun('/4 * * * *'));
-		$this->assertTrue($this->cron->shouldRun('/2 * * * *'));
-		$this->assertFalse($this->cron->shouldRun('/5 * * * *'));
-	}
+        $this->cron->testTime('2020-05-01 10:04 am');
+        $this->assertFalse($this->cron->shouldRun('10 * * * *'));
+        $this->assertTrue($this->cron->shouldRun('4 * * * *'));
+        $this->assertTrue($this->cron->shouldRun('04 * * * *'));
+        $this->assertTrue($this->cron->shouldRun('4,8 * * * *'));
+        $this->assertTrue($this->cron->shouldRun('1,2,4 * * * *'));
+        $this->assertFalse($this->cron->shouldRun('5-15 * * * *'));
+        $this->assertTrue($this->cron->shouldRun('1-5 * * * *'));
+        $this->assertTrue($this->cron->shouldRun('/4 * * * *'));
+        $this->assertTrue($this->cron->shouldRun('/2 * * * *'));
+        $this->assertFalse($this->cron->shouldRun('/5 * * * *'));
+    }
 
-	public function testHours()
-	{
-		$this->cron->testTime('2020-05-01 10:04 am');
+    public function testHours()
+    {
+        $this->cron->testTime('2020-05-01 10:04 am');
 
-		$this->assertTrue($this->cron->shouldRun('* * * * *'));
-		$this->assertTrue($this->cron->shouldRun('* 10 * * *'));
-		$this->assertFalse($this->cron->shouldRun('* 20 * * *'));
-		$this->assertTrue($this->cron->shouldRun('4 10 * * *'));
-		$this->assertFalse($this->cron->shouldRun('10 10 * * *'));
-		$this->assertTrue($this->cron->shouldRun('* 10,11 * * *'));
-		$this->assertTrue($this->cron->shouldRun('* 9,11,10 * * *'));
-		$this->assertFalse($this->cron->shouldRun('* 9,11,12 * * *'));
-		$this->assertTrue($this->cron->shouldRun('* 8-11 * * *'));
-		$this->assertFalse($this->cron->shouldRun('* 7-9 * * *'));
-		$this->assertTrue($this->cron->shouldRun('* /2 * * *'));
-		$this->assertTrue($this->cron->shouldRun('* /5 * * *'));
-		$this->assertFalse($this->cron->shouldRun('* /3 * * *'));
-	}
+        $this->assertTrue($this->cron->shouldRun('* * * * *'));
+        $this->assertTrue($this->cron->shouldRun('* 10 * * *'));
+        $this->assertFalse($this->cron->shouldRun('* 20 * * *'));
+        $this->assertTrue($this->cron->shouldRun('4 10 * * *'));
+        $this->assertFalse($this->cron->shouldRun('10 10 * * *'));
+        $this->assertTrue($this->cron->shouldRun('* 10,11 * * *'));
+        $this->assertTrue($this->cron->shouldRun('* 9,11,10 * * *'));
+        $this->assertFalse($this->cron->shouldRun('* 9,11,12 * * *'));
+        $this->assertTrue($this->cron->shouldRun('* 8-11 * * *'));
+        $this->assertFalse($this->cron->shouldRun('* 7-9 * * *'));
+        $this->assertTrue($this->cron->shouldRun('* /2 * * *'));
+        $this->assertTrue($this->cron->shouldRun('* /5 * * *'));
+        $this->assertFalse($this->cron->shouldRun('* /3 * * *'));
+    }
 
-	public function testMonthDay()
-	{
-		$this->cron->testTime('2020-05-01 10:04 am');
+    public function testMonthDay()
+    {
+        $this->cron->testTime('2020-05-01 10:04 am');
 
-		$this->assertTrue($this->cron->shouldRun('* * 1 * *'));
-		$this->assertTrue($this->cron->shouldRun('* * 01 * *'));
-		$this->assertFalse($this->cron->shouldRun('* * 02 * *'));
-		$this->assertTrue($this->cron->shouldRun('04 10 1 * *'));
-		$this->assertFalse($this->cron->shouldRun('05 10 1 * *'));
-		$this->assertFalse($this->cron->shouldRun('04 11 1 * *'));
-		$this->assertTrue($this->cron->shouldRun('* * 1,2 * *'));
-		$this->assertFalse($this->cron->shouldRun('* * 3,2 * *'));
-		$this->assertTrue($this->cron->shouldRun('* * 1-3 * *'));
-		$this->assertFalse($this->cron->shouldRun('* * 3-5 * *'));
-		$this->assertTrue($this->cron->shouldRun('* * /1 * *'));
-		$this->assertFalse($this->cron->shouldRun('* * /2 * *'));
-	}
+        $this->assertTrue($this->cron->shouldRun('* * 1 * *'));
+        $this->assertTrue($this->cron->shouldRun('* * 01 * *'));
+        $this->assertFalse($this->cron->shouldRun('* * 02 * *'));
+        $this->assertTrue($this->cron->shouldRun('04 10 1 * *'));
+        $this->assertFalse($this->cron->shouldRun('05 10 1 * *'));
+        $this->assertFalse($this->cron->shouldRun('04 11 1 * *'));
+        $this->assertTrue($this->cron->shouldRun('* * 1,2 * *'));
+        $this->assertFalse($this->cron->shouldRun('* * 3,2 * *'));
+        $this->assertTrue($this->cron->shouldRun('* * 1-3 * *'));
+        $this->assertFalse($this->cron->shouldRun('* * 3-5 * *'));
+        $this->assertTrue($this->cron->shouldRun('* * /1 * *'));
+        $this->assertFalse($this->cron->shouldRun('* * /2 * *'));
+    }
 
-	public function testMonth()
-	{
-		$this->cron->testTime('2020-05-01 10:04 am');
+    public function testMonth()
+    {
+        $this->cron->testTime('2020-05-01 10:04 am');
 
-		$this->assertTrue($this->cron->shouldRun('* * * 5 *'));
-		$this->assertFalse($this->cron->shouldRun('* * * 6 *'));
-		$this->assertTrue($this->cron->shouldRun('* * * 5,6 *'));
-		$this->assertFalse($this->cron->shouldRun('* * * 4,6 *'));
-		$this->assertTrue($this->cron->shouldRun('* * * 4-6 *'));
-		$this->assertFalse($this->cron->shouldRun('* * * 6-8 *'));
-		$this->assertTrue($this->cron->shouldRun('* * * /5 *'));
-		$this->assertFalse($this->cron->shouldRun('* * * /2 *'));
-	}
+        $this->assertTrue($this->cron->shouldRun('* * * 5 *'));
+        $this->assertFalse($this->cron->shouldRun('* * * 6 *'));
+        $this->assertTrue($this->cron->shouldRun('* * * 5,6 *'));
+        $this->assertFalse($this->cron->shouldRun('* * * 4,6 *'));
+        $this->assertTrue($this->cron->shouldRun('* * * 4-6 *'));
+        $this->assertFalse($this->cron->shouldRun('* * * 6-8 *'));
+        $this->assertTrue($this->cron->shouldRun('* * * /5 *'));
+        $this->assertFalse($this->cron->shouldRun('* * * /2 *'));
+    }
 
-	public function testWeekDay()
-	{
-		// May 1 is s Friday
-		$this->cron->testTime('2020-05-01 10:04 am');
+    public function testWeekDay()
+    {
+        // May 1 is s Friday
+        $this->cron->testTime('2020-05-01 10:04 am');
 
-		$this->assertTrue($this->cron->shouldRun('* * * * 5'));
-		$this->assertFalse($this->cron->shouldRun('* * * * 6'));
-		$this->assertTrue($this->cron->shouldRun('* * * * 5,6'));
-		$this->assertFalse($this->cron->shouldRun('* * * * 4,6'));
-		$this->assertFalse($this->cron->shouldRun('* * * * 1-3'));
-		$this->assertTrue($this->cron->shouldRun('* * * * 4-6'));
-		$this->assertTrue($this->cron->shouldRun('* * * * /5'));
-		$this->assertFalse($this->cron->shouldRun('* * * * /2'));
-	}
+        $this->assertTrue($this->cron->shouldRun('* * * * 5'));
+        $this->assertFalse($this->cron->shouldRun('* * * * 6'));
+        $this->assertTrue($this->cron->shouldRun('* * * * 5,6'));
+        $this->assertFalse($this->cron->shouldRun('* * * * 4,6'));
+        $this->assertFalse($this->cron->shouldRun('* * * * 1-3'));
+        $this->assertTrue($this->cron->shouldRun('* * * * 4-6'));
+        $this->assertTrue($this->cron->shouldRun('* * * * /5'));
+        $this->assertFalse($this->cron->shouldRun('* * * * /2'));
+    }
 
-	public function testHoursAndMins()
-	{
-		$this->cron->testTime('6:30 PM');
-		$this->assertTrue($this->cron->shouldRun('30 18 * * *'));
-	}
+    public function testHoursAndMins()
+    {
+        $this->cron->testTime('6:30 PM');
+        $this->assertTrue($this->cron->shouldRun('30 18 * * *'));
+    }
 
-	/**
-	 * @dataProvider hoursProvider
-	 */
-	public function testEveryHour($hourTrue, $hourFalse)
-	{
-		$this->cron->testTime($hourTrue);
-		$this->assertTrue($this->cron->shouldRun('00 * * * *'));
+    /**
+     * @dataProvider hoursProvider
+     */
+    public function testEveryHour($hourTrue, $hourFalse)
+    {
+        $this->cron->testTime($hourTrue);
+        $this->assertTrue($this->cron->shouldRun('00 * * * *'));
 
-		$this->cron->testTime($hourFalse);
-		$this->assertFalse($this->cron->shouldRun('00 * * * *'));
-	}
+        $this->cron->testTime($hourFalse);
+        $this->assertFalse($this->cron->shouldRun('00 * * * *'));
+    }
 
-	public function testQuarterPastHour()
-	{
-		$this->cron->testTime('6:15 PM');
-		$this->assertTrue($this->cron->shouldRun('15 * * * *'));
+    public function testQuarterPastHour()
+    {
+        $this->cron->testTime('6:15 PM');
+        $this->assertTrue($this->cron->shouldRun('15 * * * *'));
 
-		$this->cron->testTime('6:30 PM');
-		$this->assertFalse($this->cron->shouldRun('15 * * * *'));
-	}
+        $this->cron->testTime('6:30 PM');
+        $this->assertFalse($this->cron->shouldRun('15 * * * *'));
+    }
 
-	public function testSpecificDate()
-	{
-		$this->cron->testTime('January 1, 2020 12:30 am');
-		$this->assertTrue($this->cron->shouldRun('30 0 1 1,6,12 *'));
+    public function testSpecificDate()
+    {
+        $this->cron->testTime('January 1, 2020 12:30 am');
+        $this->assertTrue($this->cron->shouldRun('30 0 1 1,6,12 *'));
 
-		$this->cron->testTime('June 1, 2020 12:30 am');
-		$this->assertTrue($this->cron->shouldRun('30 0 1 1,6,12 *'));
+        $this->cron->testTime('June 1, 2020 12:30 am');
+        $this->assertTrue($this->cron->shouldRun('30 0 1 1,6,12 *'));
 
-		$this->cron->testTime('December 1, 2020 12:30 am');
-		$this->assertTrue($this->cron->shouldRun('30 0 1 1,6,12 *'));
+        $this->cron->testTime('December 1, 2020 12:30 am');
+        $this->assertTrue($this->cron->shouldRun('30 0 1 1,6,12 *'));
 
-		$this->cron->testTime('February 1, 2020 12:30 am');
-		$this->assertFalse($this->cron->shouldRun('30 0 1 1,6,12 *'));
-	}
+        $this->cron->testTime('February 1, 2020 12:30 am');
+        $this->assertFalse($this->cron->shouldRun('30 0 1 1,6,12 *'));
+    }
 
-	public function testEveryWeekdayInMonth()
-	{
-		// October 5th is a Monday
-		$this->cron->testTime('October 5, 2020 8:00 pm');
-		$this->assertTrue($this->cron->shouldRun('0 20 * 10 1-5'));
+    public function testEveryWeekdayInMonth()
+    {
+        // October 5th is a Monday
+        $this->cron->testTime('October 5, 2020 8:00 pm');
+        $this->assertTrue($this->cron->shouldRun('0 20 * 10 1-5'));
 
-		$this->cron->testTime('October 4, 2020 8:00 pm');
-		$this->assertFalse($this->cron->shouldRun('0 20 * 10 1-5'));
+        $this->cron->testTime('October 4, 2020 8:00 pm');
+        $this->assertFalse($this->cron->shouldRun('0 20 * 10 1-5'));
 
-		// Another Monday
-		$this->cron->testTime('November 2, 2020 8:00 pm');
-		$this->assertFalse($this->cron->shouldRun('0 20 * 10 1-5'));
-	}
+        // Another Monday
+        $this->cron->testTime('November 2, 2020 8:00 pm');
+        $this->assertFalse($this->cron->shouldRun('0 20 * 10 1-5'));
+    }
 
-	public function hoursProvider()
-	{
-		$hours24 = array_map(function ($h) {
-			return [
-				$h . ':00',
-				$h . ':10',
-			];
-		}, range(0, 23));
-		$hoursAM = array_map(function ($h) {
-			return [
-				$h . ':00 AM',
-				$h . ':10 AM',
-			];
-		}, range(1, 12));
-		$hoursPM = array_map(function ($h) {
-			return [
-				$h . ':00 PM',
-				$h . ':10 PM',
-			];
-		}, range(1, 12));
-		return array_merge($hours24, $hoursAM, $hoursPM);
-	}
+    public function hoursProvider()
+    {
+        $hours24 = array_map(function ($h) {
+            return [
+                $h . ':00',
+                $h . ':10',
+            ];
+        }, range(0, 23));
+        $hoursAM = array_map(function ($h) {
+            return [
+                $h . ':00 AM',
+                $h . ':10 AM',
+            ];
+        }, range(1, 12));
+        $hoursPM = array_map(function ($h) {
+            return [
+                $h . ':00 PM',
+                $h . ':10 PM',
+            ];
+        }, range(1, 12));
+        return array_merge($hours24, $hoursAM, $hoursPM);
+    }
+
+    public function nextRunProvider()
+    {
+        return [
+//          ['* * * * *', 'October 5, 2020 8:01 pm'],
+//          ['4 * * * *', 'October 5, 2020 8:04 pm'],
+//          ['5-10 * * * *', 'October 5, 2020 8:05 pm'],
+//          ['57,3,8,10 * * * *', 'October 5, 2020 8:03 pm'],
+//          ['/5 * * * *', 'October 5, 2020 8:05 pm'],
+//          ['30/5 * * * *', 'October 5, 2020 8:30 pm'],
+//          ['* 6 * * *', 'October 6, 2020 6:00 am'],
+//          ['* 12-14 * * *', 'October 6, 2020 12:00 pm'],
+//          ['* 5,6 * * *', 'October 6, 2020 5:00 am'],
+//          ['* 2/4 * * *', 'October 5, 2020 10:00 pm'],
+//          ['5 10 * * *', 'October 6, 2020 10:05 am'],
+//          ['* * 10 * *', 'October 10, 2020 8:00 pm'],
+//          ['5 4 10 * *', 'October 10, 2020 4:05 am'],
+//          ['* * * 3 *', 'March 5, 2021 8:00 pm'],
+//          ['* 4/5 12 3 *', 'March 12, 2021 4:00 am'],
+//          ['* * * * Wed', 'October 6, 2020 8:00 pm'],
+//          ['* * * * 3', 'October 6, 2020 8:00 pm'],
+            ['* * * * 6,0', 'October 10, 2020 8:00 pm'],
+        ];
+    }
+
+    /**
+     * @dataProvider nextRunProvider
+     */
+    public function testNextRun(string $exp, string $expected)
+    {
+        $this->cron->testTime('October 5, 2020 8:00 pm');
+
+        $next = $this->cron->nextRun($exp);
+
+        $this->assertInstanceOf(\CodeIgniter\I18n\Time::class, $next);
+        $this->assertEquals($expected, $next->format('F j, Y g:i a'));
+    }
 }
