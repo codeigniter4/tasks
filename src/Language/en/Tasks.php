a47079f942c55ca4b41c57f01c2aa2577e6af4d1
Pooya Parsa <pooya_parsa_dadashi@yahoo.com>
2023-08-11 05:19:29 +0330

    fix: remove namespace from lang

--- src/Language/en/Tasks.php
+++ src/Language/en/Tasks.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace CodeIgniter\Tasks\Language\en;
-
 return [
     'invalidTaskType'       => '"{0}" is not a valid type of task.',
     'invalidCronExpression' => '"{0}" is not a valid cron expression.',
